a66a673b123915d7255148acc79e9ec7104bbc25
lushenyu <lushenyu@tencent.com>
2021-08-10 20:32:43 +0800

    chore: 代码调试

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+// import './App.css'
 // import './css/style-login.scss'
+import "./img/test.scss";
 import Button from './components/Button'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header login-warp">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo}  className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,32 @@ export default defineConfig({
   plugins: [reactRefresh(), injectReact()],
   server: {
     port: 8080
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        importer: [
+          // This importer uses the synchronous API, and can be passed to either
+          // renderSync() or render().
+          function(url, prev) {
+            // This generates a stylesheet from scratch for `@use "big-headers"`.
+            console.log('importerimporter', url, prev)
+            if (url != "big-headers") return null;
+            return null
+          },
+
+          // This importer uses the asynchronous API, and can only be passed to
+          // render().
+          function(url, prev, done) {
+            // Convert `@use "foo/bar"` to "node_modules/foo/sass/bar".
+            var components = url.split('/');
+            var innerPath = components.slice(1).join('/');
+            done({
+              file: `node_modules/${components.first}/sass/${innerPath}`
+            });
+          }
+        ]
+      }
+    }
   }
 })
